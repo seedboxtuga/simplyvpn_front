5c453fc48b49912c9072183eab0c252148dead31
seedboxtuga <89316920+seedboxtuga@users.noreply.github.com>
2025-09-25 19:15:53 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 import { GeistSans } from "geist/font/sans"
 import { GeistMono } from "geist/font/mono"
+import { Analytics } from "@vercel/analytics/next"
 import MiniKitProvider from "@/components/minikit-provider"
 import "./globals.css"
 import { Suspense } from "react"
@@ -23,6 +24,7 @@ export default function RootLayout({
         <Suspense fallback={null}>
           <MiniKitProvider>{children}</MiniKitProvider>
         </Suspense>
+        <Analytics />
       </body>
     </html>
   )
